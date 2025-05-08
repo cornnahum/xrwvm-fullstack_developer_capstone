3b8f9dbe6ced7200bb785ec351005a70943a4609
Cornilious Nolasco <115691686+cornnahum@users.noreply.github.com>
2025-05-08 16:26:40 +0200

    Added get_cars view and imported CarMake, CarModel

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,7 +13,6 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
 from .models import CarMake, CarModel
 from .populate import initiate
 
